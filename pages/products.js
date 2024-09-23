7eaedcecaf95d2e6820c100d1ce0aea269f0aaf3
Rohit Sahu <sahu86744@gmail.com>
2024-09-23 23:03:13 +0530

    updated code

--- pages/products.js
+++ pages/products.js
@@ -1,12 +1,12 @@
 import Header from "@/components/Header";
 import styled from "styled-components";
 import Center from "@/components/Center";
-import {mongooseConnect} from "@/lib/mongoose";
-import {Product} from "@/models/Product";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 import ProductsGrid from "@/components/ProductsGrid";
 import Title from "@/components/Title";
 
-export default function ProductsPage({products}) {
+export default function ProductsPage({ products }) {
   return (
     <>
       <Header />
@@ -20,9 +20,9 @@ export default function ProductsPage({products}) {
 
 export async function getServerSideProps() {
   await mongooseConnect();
-  const products = await Product.find({}, null, {sort:{'_id':-1}});
+  const products = await product?.find({}, null, { sort: { '_id': -1 } });
   return {
-    props:{
+    props: {
       products: JSON.parse(JSON.stringify(products)),
     }
   };
