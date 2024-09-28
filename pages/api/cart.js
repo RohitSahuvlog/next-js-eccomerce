d60a3a42b2e19747fe2618bfbb926340ffa3076f
Rohit Sahu <sahu86744@gmail.com>
2024-09-28 20:02:22 +0530

    added method

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -2,7 +2,11 @@ import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 
 export default async function handle(req, res) {
-  await mongooseConnect();
-  const ids = req.body.ids;
-  res.json(await Product?.find({ _id: ids }));
+  if (req.method === 'POST') { // Check if the request is a POST request
+    await mongooseConnect();
+    const ids = req.body.ids;
+    res.json(await Product?.find({ _id: ids }));
+  } else {
+    res.status(405).end(); // Method Not Allowed
+  }
 }
