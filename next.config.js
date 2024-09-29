5ad5854c05d709b3a3a4f9a08f51706f8a7da47e
Rohit Sahu <sahu86744@gmail.com>
2024-09-29 11:22:23 +0530

    updated build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   trailingSlash: true,
   compiler: {
     styledComponents: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
