0bc7d1f3c17faf1842a304c8fb74abbd441699a6
Rohit Sahu <sahu86744@gmail.com>
2024-09-29 11:48:58 +0530

    fixed api

--- pages/api/products.js
+++ pages/api/products.js
@@ -3,6 +3,13 @@ import { Product } from "@/models/Product";
 
 export default async function handler(req, res) {
     await mongooseConnect();
-    const products = await Product.find({}, null, { sort: { '_id': -1 } });
-    res.status(200).json(products);
+
+    if (req.method === 'GET') {
+        if (req.query?.id) {
+            res.status(200).send(await Product.findOne({ _id: req.query.id }));
+        } else {
+            const products = await Product.find({}, null, { sort: { '_id': -1 } });
+            res.status(200).send(products);
+        }
+    }
 }
--- pages/products.js
+++ pages/products.js
@@ -4,15 +4,15 @@ import styled from "styled-components";
 import Center from "@/components/Center";
 import ProductsGrid from "@/components/ProductsGrid";
 import Title from "@/components/Title";
+import axios from "axios";
 
 export default function ProductsPage() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     async function fetchProducts() {
-      const response = await fetch('/api/products');
-      const data = await response.json();
-      setProducts(data);
+      const response = await axios.get('/api/products');
+      setProducts(response.data);
     }
 
     fetchProducts();
